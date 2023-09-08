8dc7182523aa7dd96b2a01f84bb96973dacba534
ingbinsee <ingbinsee@gmail.com>
2023-09-08 22:09:42 +0900

    [feat] react-router를 활용한 페이지 연결 - <NavLink>를 활용하여 클릭 시 해당 페이지로 전환 - svg 이미지의 크기와 색상을 속성으로 제어할 수 있도록 하여 변경을 용이하게 함

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,12 +5,17 @@ import {
 } from 'react-router-dom';
 import Home from './pages/Home';
 import Total from './pages/Total';
+import Search from './pages/Search';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<Home />} />
       <Route path="/total" element={<Total />} />
+
+      <Route path="/search" element={<Search />} />
+      <Route path="/chat" element={<Total />} />
+      <Route path="/profile" element={<Total />} />
     </>
   )
 );

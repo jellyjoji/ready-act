9ad5268376e6bb4d1d134affe255eba165d241b1
ingbinsee <ingbinsee@gmail.com>
2023-09-10 15:18:11 +0900

    [feat] 카테고리별 상품 페이지 작업 - 클라이언트 SDK filter를 활용하여 원하는 카테고리 선택 - 렌더링

---
+++ src/pages/Products/Grains.jsx
@@ -0,0 +1,99 @@
+import {pb} from '@/api/pocketbase';
+import participateNum from '@/assets/icons/participateNum.svg';
+import pickuptime from '@/assets/icons/pickuptime.svg';
+import prev from '@/assets/icons/prev.svg';
+import {useEffect} from 'react';
+import {Helmet} from 'react-helmet-async';
+import {Link} from 'react-router-dom';
+
+pb.autoCancellation(false);
+
+const filterRecordList = await pb.collection('products').getList(1, 50, {
+  filter: 'category="🌽 곡류"',
+});
+
+function Grains() {
+  const filterRecordItems = filterRecordList.items;
+
+  useEffect(() => {
+    try {
+      filterRecordItems;
+    } catch (error) {
+      throw new Error('error');
+    }
+  }, [filterRecordItems]);
+
+  return (
+    <>
+      <Helmet>
+        <title>R09M - 전체 상품</title>
+      </Helmet>
+
+      <div className="bg-line-200 py-2">
+        <Link to="/r09m">
+          <img src={prev} alt="뒤로 가기" className="p-4" />
+        </Link>
+        <ul>
+          {filterRecordItems.map(
+            ({
+              id,
+              category,
+              status,
+              title,
+              content,
+              pickup,
+              participate,
+              participateNumber,
+            }) => (
+              <Link to={id} key={id}>
+                <li className=" rounded-2xl p-5 m-6 bg-white">
+                  <span className="font-semibold bg-line-400 text-greenishgray-800 p-2 rounded-xl">
+                    {category}
+                  </span>
+                  <div className="relative mb-4">
+                    {status === '대기중' ? (
+                      <span className="font-bold absolute text-primary-500">
+                        {status}
+                      </span>
+                    ) : status === '진행중' ? (
+                      <span className="font-bold absolute text-map-500">
+                        {status}
+                      </span>
+                    ) : (
+                      <span className="font-bold absolute text-greenishgray-500">
+                        {status}
+                      </span>
+                    )}
+                    <h2 className="text-greenishgray-700 font-semibold mt-5 ml-20">
+                      {title}
+                    </h2>
+                    <p className="text-sm my-2">{content}</p>
+                  </div>
+                  <div className="flex gap-2 justify-end text-xs text-greenishgray-600">
+                    <div className="flex gap-1">
+                      <img src={pickuptime} alt="픽업 시간" className="w-4" />
+                      <span>{pickup.slice(5, -8).replace('-', '/')}</span>
+                    </div>
+                    <div className="flex gap-1">
+                      <img
+                        src={participateNum}
+                        alt="참여 인원"
+                        className="w-4"
+                      />
+                      <span>
+                        {participate.length}/{participateNumber}
+                      </span>
+                    </div>
+                  </div>
+                </li>
+              </Link>
+            )
+          )}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Grains;
+
--- src/routes.jsx
+++ src/routes.jsx
@@ -6,7 +6,12 @@ import {
 import Home from './pages/Home';
 import Logo from './pages/Logo';
 import Search from './pages/Search';
-import Total from './pages/Total';
+import Total from './pages/Products/Total';
+import Vegetable from './pages/Products/Vegetable';
+import Fruit from './pages/Products/Fruit';
+import Grains from './pages/Products/Grains';
+import Meat from './pages/Products/Meat';
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -14,6 +19,10 @@ const router = createBrowserRouter(
       <Route path="/" element={<Logo />} />
       <Route path="/r09m" element={<Home />} />
       <Route path="r09m/products/total" element={<Total />} />
+      <Route path="r09m/products/vegetable" element={<Vegetable />} />
+      <Route path="r09m/products/fruit" element={<Fruit />} />
+      <Route path="r09m/products/grains" element={<Grains />} />
+      <Route path="r09m/products/meat" element={<Meat />} />
 
       <Route path="search" element={<Search />} />
       <Route path="/chat" element={<Total />} />

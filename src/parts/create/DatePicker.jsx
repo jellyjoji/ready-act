4b429bf0e45baae72e12b24b5573bfc20a4c446b
DESKTOP-TT2DN3I\User <dirnrhd21@naver.com>
2023-09-18 17:51:50 +0900

    [refactor] 방만들기 페이지 폴더 정리 - pull로 당겨온 후 파일 정리

---
+++ src/parts/create/DatePicker.jsx
@@ -0,0 +1,17 @@
+
+import { useState, forwardRef, useId, } from "react";
+// import Input from "@/components/Input";
+
+function DatePicker({ title, className, labelClassName, ...restProps }, ref) {
+  const [date, setDate] = useState(null);
+  const id = useId();
+
+  return (
+    <div>
+      <label htmlFor={id} className={labelClassName}>{title}</label>
+      <input id={id} ref={ref} className={className} type="date" onChange={(e) => setDate(e.target.value)} {...restProps} />
+    </div>
+  );
+};
+
+export default forwardRef(DatePicker);
